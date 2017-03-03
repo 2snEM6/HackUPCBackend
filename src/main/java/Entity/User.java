810162d4bd7dc1970cd6ab79357ac46040e9b721
Suufian <soufian.ben.bouker@est.fib.upc.edu>
2017-03-04 00:45:58 +0100

    User entity

---
+++ src/main/java/Entity/User.java
@@ -0,0 +1,55 @@
+package Entity;
+
+/**
+ * Created by admin on 04/03/2017.
+ */
+public class User {
+
+    private Integer userId;
+    private String name;
+    private String email;
+    private String signUpDate;
+
+    public User() {
+    }
+
+    public User(Integer userId, String name, String email, String signUpDate) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.signUpDate = signUpDate;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSignUpDate() {
+        return signUpDate;
+    }
+
+    public void setSignUpDate(String signUpDate) {
+        this.signUpDate = signUpDate;
+    }
+
+}
---
+++ src/main/java/Handler/UserHandler.java
@@ -0,0 +1,9 @@
+package Handler;
+
+/**
+ * Created by admin on 04/03/2017.
+ */
+public class UserHandler {
+
+
+}
